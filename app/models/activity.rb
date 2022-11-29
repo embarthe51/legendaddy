976b8732f0145bfcd05cb1b4388657d843b8b1b6
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2022-11-29 23:16:25 +0100

    ajouter une methode aux acitivities qui formate les horraires d'ouverture

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,5 +4,24 @@ class Activity < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
-  validates :title, :description, :address, :workshop, presence: true
+  validates :title, :description, :address, presence: true
+
+  def formated_open_times
+    if workshop
+      "This event takes place on: <br/> #{start_at.strftime('%d/%m/%Y')} <br/>
+      at : #{start_at.strftime('%H h %M')}"
+    else
+      "
+        Opening times <br/>
+        from : #{open_hour.hour} h #{0 if open_hour.min < 10}#{open_hour.min} <br/>
+        to : #{open_hour.hour} h #{0 if open_hour.min < 10}#{open_hour.min} <br/>
+        days : #{format_open_days(open_days).join(", ")}
+      "
+    end
+  end
+
+  def format_open_days(open_days_unformated)
+    arr_of_days = open_days_unformated.to_s.chars
+    arr_of_days.map! { |item| Date::DAYNAMES[item.to_i] }
+  end
 end
