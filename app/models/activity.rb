d5cc73e3d9e3eede03e4c2741505239fd64ee247
ElodieBou97 <didie97160.eb@gmail.com>
2022-11-29 16:47:04 +0100

    show html fini

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,6 @@
 class Activity < ApplicationRecord
+  belongs_to :user
+  has_many_attached :photos
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  belongs_to :user
 end
