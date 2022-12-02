65415a1422d50e5bb672dee89b3774ff657b388a
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2022-12-02 14:05:52 +0100

    New navbar YEAH

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :activities, only: [:index, :show] do
     resources :bookings, only: [:new, :create]
   end
+  resources :reviews, only: [:index]
   resources :bookings, only: [:index, :show, :destroy]
   resources :availabilities, only: [:index, :new, :create]
   post '/search_activities', to: 'activities#search', as: :search_activities
